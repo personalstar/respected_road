1803dda3e24a644c98be8c45574cfc6335245643
personalstar <littlestar.99@qq.com>
2021-07-26 11:11:21 +0800

    绘制图形第一弹——红色方框

---
+++ turtle_test/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/7/26 10:42 上午
+# @Author  : xin
+# @Detail  :
---
+++ turtle_test/test_pig.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/7/26 10:42 上午
+# @Author  : xin
+# @Detail  : 绘制图形第一弹：红色方框
+
+import turtle
+
+turtle.pensize(4)
+turtle.pencolor('red')
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
+turtle.mainloop()
